4f3af391f3428de49c3004d9d7dedf728a113d3e
Apurv <apurv.main2@gmail.com>
2022-01-10 20:29:19 +0530

    testing2

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,31 +1,27 @@
-import PropTypes from 'prop-types';
-import React, { useState } from 'react';
-
+import PropTypes from "prop-types";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const handleUpClick = () => {
+    // console.log("Uppercase was clicked");
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
 
-    const handleUpClick = ()=>{
-        // console.log("Uppercase was clicked");
-        let newText = text.toUpperCase();
-        setText(newText);
-    }
-    
-    // Used for taking input from user in forms
-    const handleOnChange = (event)=>{
-        // console.log("Handling change");
-        setText(event.target.value);
-    }
-    
-    const [text, setText] = useState('Enter your text here');
+  // Used for taking input from user in forms
+  const handleOnChange = (event) => {
+    // console.log("Handling change");
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("Enter your text here");
 
   return (
     <div>
       <h2>{props.heading}</h2>
 
-      
-
       <div className="form-group border border-secondary">
-          {/* Using textarea to get input from user  */}
+        {/* Using textarea to get input from user  */}
         <textarea
           className="form-control"
           value={text}
@@ -34,11 +30,14 @@ export default function TextForm(props) {
           rows="8"
         ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary" onClick={handleUpClick}>
+        Convert to Uppercase
+      </button>
     </div>
   );
 }
 
+
 TextForm.propTypes = {
-    heading: PropTypes.string.isRequired
-}
+  heading: PropTypes.string.isRequired,
+};
